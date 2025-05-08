65b3a08cc91a2fabfcf3d8ebdc703fd5d68202fc
RAYANDAVID <rayandavid25@gmail.com>
2025-05-08 11:41:38 -0300

    Mamifero

---
+++ src/Mamifero.java
@@ -0,0 +1,18 @@
+public class Mamifero {
+    //atributos
+    private int id;
+    private String nomeCientifico;
+
+    public int getid(){
+        return this.id;
+
+    }
+    public void setNomeCientifico(String nomeCientifico){
+        this.nomeCientifico = nomeCientifico;
+    }
+    public String getNomeCientifico(){
+        return this.nomeCientifico;
+    }
+}
+
+
