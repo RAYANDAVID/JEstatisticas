2677596c038a42b9b8cd030f9cf3d4b4d878aa7f
RAYANDAVID <rayandavid25@gmail.com>
2025-05-09 08:38:15 -0300

    Lista de exercicios

--- src/App.java
+++ src/App.java
@@ -1,19 +1,7 @@
-import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //Classe é um conjunto de objetos
-        //Nosso objetivo nesse código é entender a diferença
-        //entre o método com o tipo void e método com o tipo String
-
-        Mamifero m1 = new Mamifero();//objeto
-        m1.setNomeCientifico("Bos Taurus");//atribuindo valor ao objeto
-        Mamifero m2 = new Mamifero();
-        m2.setNomeCientifico("Macropus");
-        Mamifero m3 = new Mamifero();
-        m3.setNomeCientifico("Lamantins");
-        JOptionPane.showMessageDialog(null, "Nome Cientifico: "+m1.getNomeCientifico()+"\n"+m2.getNomeCientifico()+"\n"+m3.getNomeCientifico());//Mostra o resultado do valor atribuido ao objeto
-        
+      //Leia o README para saber as instruções da branch
     }
 
 }
