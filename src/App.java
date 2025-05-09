0ce17443edf2513609c770c057164331737aa5ca
RAYANDAVID <rayandavid25@gmail.com>
2025-05-09 09:43:15 -0300

    Nível 1 - Entrada e Saída

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      //Leia o README para saber as instruções da branch
+
+        JOptionPane.showMessageDialog(null, "Óla jardim!");
+        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+        String endereco = JOptionPane.showInputDialog("Digite seu endereço: ");
+        double largura = Double.parseDouble(JOptionPane.showInputDialog("Qual a largura do seus jardim (em metros): "));
+        double comp = Double.parseDouble(JOptionPane.showInputDialog("Qual o comprimento(em metros): "));
+        JOptionPane.showMessageDialog(null,
+                "Nome: " + nome + "\nEndereço: " + endereco + "\nLargura: " + largura + "\nComprimento: " + comp);
+        Double area = largura * comp;
+        JOptionPane.showMessageDialog(null, "Área do jardim: " + area);
     }
 
 }
